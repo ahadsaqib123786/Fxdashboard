f58dfdc444b54f7722f6c5c72fd8d54f9f5559c8
ahadsaqib123786 <ahadsaqib.as@gmail.com>
2026-07-10 01:32:56 +0500

    Delete fx-dashboard directory

--- fx-dashboard/components/GradeBadge.js
+++
@@ -1,9 +0,0 @@
-const GRADE_CLASS = {
-  Display: "grade-a-plus",
-  Monitor: "grade-b",
-  Ignore: "grade-ignore",
-};
-
-export default function GradeBadge({ grade }) {
-  return <span className={`grade-badge ${GRADE_CLASS[grade] || "grade-b"}`}>{grade}</span>;
-}
